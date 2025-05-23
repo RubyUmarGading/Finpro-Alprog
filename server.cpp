fb173346d9b0f891442155ed6f0d717e7fbf601b
RubyUmarGading <rubygading2005@gmail.com>
2025-05-23 13:24:33 +0700

    Adding parsing and writing to json

--- server.cpp
+++ server.cpp
@@ -4,9 +4,77 @@
 #include <bits/stdc++.h>
 #include <fstream>
 #include <vector>
+#include "json.hpp"
+
 using namespace std;
+using json = nlohmann::json;
 
 #define PORT 8888
+
+struct Data {
+    int jam;
+    int menit;
+    int level;
+};
+
+// Parsing
+vector<string> getData(string messageRecv) {
+    vector<string> fields;
+    string field;
+    for (char c : messageRecv) {
+        if (c == ':' || c == '-') {
+            fields.push_back(field);
+            field.clear();
+        } else {
+            field += c;
+        }
+    }
+    fields.push_back(field);
+    return fields;
+}
+
+// baca data
+vector<Data> readFile(string fileName) {
+    vector<Data> dataList;
+    ifstream file(fileName);
+    string line;
+
+    while (getline(file, line)) {
+        vector<string> fields = getData(line);
+        
+        Data data;
+        data.jam = stoi(fields[0]);
+        data.menit = stoi(fields[1]);
+        data.level = stoi(fields[2]);
+        dataList.push_back(data);
+        
+    }
+
+    return dataList;
+}
+
+// Masukkan ke JSON
+void writeCritical(vector<Data> dataList) {
+    json j;
+
+    for (const Data& d : dataList) {
+        if (d.level <= 10 || d.level >= 200) {
+            json entry;
+            entry["jam"] = d.jam;
+            entry["menit"] = d.menit;
+            entry["level"] = d.level;
+            j.push_back(entry);
+        }
+    }
+
+    ofstream file("critical_data.json");
+    if (file.is_open()) {
+        file << j.dump(4);
+        file.close();
+    } else {
+        cerr << "Failed to write JSON file\n";
+    }
+}
 
 int main() {
     WSADATA wsa;
@@ -48,6 +116,9 @@ int main() {
         cout << "Received message from client: " << messageRecv << endl;
     }
 
+    // Ambil data dan memasukkan data level kritikal ke JSON
+    vector<Data> Data_fix = readFile(string messageRecv);
+    writeCritical(Data_fix);
 
     closesocket(client_socket);
     closesocket(listen_socket);
